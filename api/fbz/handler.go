01779275c7a9d7c865282879b615a0e660ad5093
Steven Santee <ssantee@icloud.com>
2025-06-20 19:26:05 -0400

    remove extraneous output

--- api/fbz/handler.go
+++ api/fbz/handler.go
@@ -2,14 +2,11 @@ package fbz
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 	"strconv"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, this is the fizzbuzz API handler!\n")
-
 	userProvidedNumber := r.URL.Query().Get("number")
 	if userProvidedNumber == "" {
 		http.Error(w, "Please provide a number in the query parameter 'number'", http.StatusBadRequest)
