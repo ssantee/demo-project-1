d7c3c43b764fef037d674cbcf9ea7e290efaef3b
Steven Santee <steven.santee@marion.k12.fl.us>
2025-06-23 08:56:02 -0400

    page title

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,9 @@ import './globals.css';
 import { Analytics } from '@vercel/analytics/react';
 
 export const metadata = {
-  title: 'Next.js App Router + NextAuth + Tailwind CSS',
+  title: 'Demo By Steven Santee',
   description:
-    'A user admin dashboard configured with Next.js, Postgres, NextAuth, Tailwind CSS, TypeScript, and Prettier.'
+    'Two custom tools for generating FizzBuzz and Fibonacci sequences.'
 };
 
 export default function RootLayout({
