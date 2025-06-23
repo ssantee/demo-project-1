5539577f1b3b974a80bad47f531a15ad8198cc68
Steven Santee <steven.santee@marion.k12.fl.us>
2025-06-23 08:56:26 -0400

    formatting, unused class

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,25 +1,25 @@
 import { Analytics } from '@vercel/analytics/react';
 import Providers from './providers';
 
-import { SidebarTrigger } from "@/components/ui/sidebar"
-import { AppSidebar } from "@/components/app-sidebar"
+import { SidebarTrigger } from '@/components/ui/sidebar';
+import { AppSidebar } from '@/components/app-sidebar';
 export default function DashboardLayout({
-  children
+    children
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-  return (
-    <Providers>
-      <main className="flex min-h-screen w-full flex-row bg-muted/40">
-        <AppSidebar />
-        <div className="flex flex-col grow sm:gap-4 sm:py-4 sm:pl-14 md:pl-0">
-          <main className="flex-1 items-start gap-2 p-4 sm:px-6 sm:py-0 md:gap-4 bg-muted/40 w-auto">
-            <SidebarTrigger />
-            {children}
-          </main>
-        </div>
-        <Analytics />
-      </main>
-    </Providers>
-  );
+    return (
+        <Providers>
+            <main className="flex min-h-screen w-full flex-row bg-muted/40">
+                <AppSidebar />
+                <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14 md:pl-0">
+                    <main className="flex-1 items-start gap-2 p-4 sm:px-6 sm:py-0 md:gap-4 bg-muted/40 w-auto">
+                        <SidebarTrigger />
+                        {children}
+                    </main>
+                </div>
+                <Analytics />
+            </main>
+        </Providers>
+    );
 }
