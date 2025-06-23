322678820dd04946f145d14ce0ba50d3ad7a03d0
Steven Santee <steven.santee@marion.k12.fl.us>
2025-06-23 09:03:48 -0400

    responsive work

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -12,7 +12,7 @@ export default function DashboardLayout({
         <Providers>
             <main className="flex min-h-screen w-full flex-row bg-muted/40">
                 <AppSidebar />
-                <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14 md:pl-0">
+                <div className="flex flex-col grow sm:gap-4 sm:py-4 md:pl-0">
                     <main className="flex-1 items-start gap-2 p-4 sm:px-6 sm:py-0 md:gap-4 bg-muted/40 w-auto">
                         <SidebarTrigger />
                         {children}
