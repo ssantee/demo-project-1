7a7a56536c363b276f2732a3943b38441ef2baea
Steven Santee <ssantee@icloud.com>
2025-06-30 18:04:25 -0400

    fix?

--- api/fib/handler.go
+++ api/fib/handler.go
@@ -9,6 +9,8 @@ import (
 
 var pregeneratedSequence = []int{0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811, 514229, 832040}
 
+const maxPregenerated = 1000000
+
 func Handler(w http.ResponseWriter, r *http.Request) {
 	userProvidedNumber := r.URL.Query().Get("n")
 	if userProvidedNumber == "" {
@@ -42,7 +44,6 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// Convert user-provided number to integer
-	userProvidedNumber = r.URL.Query().Get("n")
 	n, err := strconv.Atoi(userProvidedNumber)
 	if err != nil {
 		http.Error(w, "Invalid number provided", http.StatusBadRequest)
@@ -70,12 +71,12 @@ func fibonacci(n int, startx int, starty int) ([]int, error) {
 		return nil, errors.New("invalid input, n, startx, and starty must be non-negative integers")
 	}
 
-	if n >= 1000000 {
-		return pregeneratedSequence, nil
-	}
-
 	// For standard start location, just return a chunk of the pregenerated sequence.
 	if startx == 0 && starty == 1 {
+		if n >= maxPregenerated {
+			return pregeneratedSequence, nil
+		}
+
 		for i, e := range pregeneratedSequence {
 			if e > n {
 				return pregeneratedSequence[:i], nil
@@ -95,8 +96,8 @@ func fibonacci(n int, startx int, starty int) ([]int, error) {
 	// start at 2 because the first two already exist
 	for i := 2; i <= n; i++ {
 		next := result[i-1] + result[i-2]
-		if next > n || next > 1000000 {
-			// If the next Fibonacci number exceeds n or 1,000,000, stop.
+		if next > n || next > maxPregenerated {
+			// If the next Fibonacci number exceeds n or maxPregenerated stop.
 			break
 		}
 		result = append(result, next)
